34f2e6f345e87feea3e8bf54c04bddee8a20d9f9
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2021-12-11 11:01:54 +0530

    Uno code successfully compiled

---
+++ SPI Protocol/SPI_ArduinoUNO/Debug/VM_DBG/VM_DBG.cpp.d
@@ -0,0 +1,14 @@
+C:\Users\sukri\AppData\Local\Temp\VMBuilds\SPI_ArduinoUNO\uno\Debug\VM_DBG\VM_DBG.cpp.o: \
+ c:\program\ files\microsoft\ visual\ studio\2022\preview\common7\ide\extensions\itelwv1j.wbw\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.cpp \
+ c:\program\ files\microsoft\ visual\ studio\2022\preview\common7\ide\extensions\itelwv1j.wbw\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
--- SPI Protocol/SPI_ArduinoUNO/SPI_ArduinoUNO.ino
+++ SPI Protocol/SPI_ArduinoUNO/SPI_ArduinoUNO.ino
@@ -1,15 +1,42 @@
-/*
- Name:		SPI_ArduinoUNO.ino
- Created:	12/11/2021 10:54:14 AM
- Author:	suk
-*/
+#include <SPI.h>
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+char buff[100];
+volatile byte index;
+volatile bool receivedone;  /* use reception complete flag */
 
+void setup(void)
+{
+    Serial.begin(9600);
+    SPCR |= bit(SPE);         /* Enable SPI */
+    pinMode(MISO, OUTPUT);    /* Make MISO pin as OUTPUT */
+    index = 0;
+    receivedone = false;
+    SPI.attachInterrupt();    /* Attach SPI interrupt */
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop(void)
+{
+    if (receivedone)          /* Check and print received buffer if any */
+    {
+        buff[index] = 0;
+        Serial.println(buff);
+        index = 0;
+        receivedone = false;
+    }
+}
+
+// SPI interrupt routine
+ISR(SPI_STC_vect)
+{
+    uint8_t oldsrg = SREG;
+    cli();
+    char c = SPDR;
+    if (index < sizeof buff)
+    {
+        buff[index++] = c;
+        if (c == '\n') {     /* Check for newline character as end of msg */
+            receivedone = true;
+        }
+    }
+    SREG = oldsrg;
 }
