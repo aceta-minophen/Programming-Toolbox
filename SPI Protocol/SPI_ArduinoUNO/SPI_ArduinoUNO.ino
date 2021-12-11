7a277d12e7b95e2a69533975b4ffbcc0870e894f
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2021-12-11 10:55:06 +0530

    Created SPI Arduino Code

---
+++ SPI Protocol/SPI_ArduinoUNO/SPI_ArduinoUNO.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SPI_ArduinoUNO.ino
+ Created:	12/11/2021 10:54:14 AM
+ Author:	suk
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
