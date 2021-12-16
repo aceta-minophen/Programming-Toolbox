eb226e393c1aa6535dab6c403b0a84e1a176ae20
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2021-12-16 11:01:29 +0530

    bluetooth

---
+++ Bluetooth_hc05/Debug/Bluetooth_hc05.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\sukri\AppData\Local\Temp\VMBuilds\Bluetooth_hc05\uno\Debug\Bluetooth_hc05.cpp.o: \
+ C:\Users\sukri\AppData\Local\Temp\VMBuilds\Bluetooth_hc05\uno\Debug\Bluetooth_hc05.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ c:\program\ files\microsoft\ visual\ studio\2022\preview\common7\ide\extensions\itelwv1j.wbw\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
---
+++ Bluetooth_hc05/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\sukri\AppData\Local\Temp\VMBuilds\Bluetooth_hc05\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\microsoft\ visual\ studio\2022\preview\common7\ide\extensions\itelwv1j.wbw\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
